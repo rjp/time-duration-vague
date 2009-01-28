2be23640fcbf49ead069c72997cee46e79697815
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-28 18:39:07 +0000

    - more tests - more vague durations

--- time-duration-vague.rb
+++ time-duration-vague.rb
@@ -1,8 +1,15 @@
 class Time
 	class Duration
 		def Duration.vague(x)
+			minutes = x / 60
+			next_min = 1+minutes
+			seconds = x % 60
+			nearby = (seconds + 15) % 60
 			case 
-				when x < 60: "#{x} second#{x!=1?'s':''}"
+				when x < 30: "just now"
+				when seconds > 45: "almost #{next_min} minutes"
+				when seconds < 15: "just over #{minutes} minutes"
+				else "about #{minutes} and a half minutes"
 			end
 		end
 	end
