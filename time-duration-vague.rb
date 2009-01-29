235d59b1aa241b64162ac64f0a68912774f3813c
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-29 00:06:37 +0000

    - many more vague durations - fix up "1 minutes" into "1 minute"

--- time-duration-vague.rb
+++ time-duration-vague.rb
@@ -5,12 +5,17 @@ class Time
 			next_min = 1+minutes
 			seconds = x % 60
 			nearby = (seconds + 15) % 60
-			case 
-				when x < 30: "just now"
-				when seconds > 45: "almost #{next_min} minutes"
-				when seconds < 15: "just over #{minutes} minutes"
+			d = case
+				when x <= 20: "just now"
+				when x <= 40: "about half a minute"
+				when seconds < 5: "#{minutes} minutes"
+				when seconds > 55: "#{next_min} minutes"
+				when seconds >= 40: "almost #{next_min} minutes"
+				when seconds <= 20: "just over #{minutes} minutes"
 				else "about #{minutes} and a half minutes"
 			end
+			d.gsub!(%r{(^|\D)1 minutes}, '\1'+"1 minute")
+			return d
 		end
 	end
 end
