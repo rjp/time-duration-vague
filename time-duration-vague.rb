46d729b6404604345c6e7793bf78058e927d3852
U-FABIONUDIBRANCH\rjp <zimpenfish@gmail.com>
2009-01-29 23:15:44 +0000

    - tweaking of percentages - still not happy with 20-39%

--- time-duration-vague.rb
+++ time-duration-vague.rb
@@ -17,13 +17,13 @@ class Time
 				pcage = self.ratio(x, duration)
 				if (units > 0) or (units == 0 and pcage > 85) then
 					d = case
-						when pcage < 6: "about #{units} #{name}"
-						when pcage <= 10: "just over #{units} #{name}"
-                        when pcage < 35: "#{units} {name}"
-                        when pcage > 35 && pcage < 65: "about #{units} and a half #{name}"
-                        when pcage >= 65: "closer to #{next_min} #{name}"
-						when pcage >= 90: "almost #{next_min} #{name}"
-						when pcage > 94: "about #{next_min} #{name}"
+						when pcage < 10: "about #{units} #{name}"
+						when pcage < 20: "just over #{units} #{name}"
+                        when pcage < 40: "more than #{units} #{name}"
+                        when pcage < 60: "about #{units} and a half #{name}"
+                        when pcage < 80: "closer to #{next_min} #{name}"
+						when pcage < 90: "almost #{next_min} #{name}"
+                        when pcage >= 90: "about #{next_min} #{name}"
 					end
 					d.gsub!(%r{(^|\D)1 #{name}}, '\1'+"1 #{name.chop}")
 				end
